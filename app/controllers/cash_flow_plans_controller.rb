1f889f3b6f420747216be401cdec942ed4ee5fa8
Andrew Kennedy <andrew@jackrussellsoftware.com>
2011-10-17 22:14:27 -0400

    working with DocRaptor

--- app/controllers/cash_flow_plans_controller.rb
+++ app/controllers/cash_flow_plans_controller.rb
@@ -11,7 +11,11 @@ class CashFlowPlansController < ApplicationController
 
   def show
     authorize! :read, @cash_flow_plan
-    respond_with @cash_flow_plan
+    respond_to do |format|
+      format.html
+      format.xls { doc_raptor_send }
+      format.pdf { doc_raptor_send }
+    end
   end
 
   def new
@@ -72,6 +76,24 @@ private
   def build_cash_flow_plan_items
     CashFlowPlanItem::ITEMS.each do |ti|
       @cash_flow_plan.cash_flow_plan_items.build(:name => ti[:name], :category => ti[:category])
+    end
+  end
+  
+  def doc_raptor_send(options = { })
+    default_options = {
+      :name             => controller_name,
+      :document_type    => request.format.to_sym,
+      :test             => ! Rails.env.production?,
+    }
+    options = default_options.merge(options)
+    options[:document_content] ||= render_to_string
+    ext = options[:document_type].to_sym
+
+    response = DocRaptor.create(options)
+    if response.code == 200
+      send_data response, :filename => "#{options[:name]}.#{ext}", :type => ext
+    else
+      render :inline => response.body, :status => response.code
     end
   end
 end
