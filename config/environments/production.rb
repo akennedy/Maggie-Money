b1e7a457cad3ca023585dec5309e7e9b633f30c5
Andrew Kennedy <andrew@jackrussellsoftware.com>
2012-04-06 23:13:05 -0400

    update email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ MaggieMoney::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'maggiemoney.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'maggiemoney.com' }
 
   # Enable threaded mode
   # config.threadsafe!
